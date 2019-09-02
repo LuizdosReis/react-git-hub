1f62050638852ee41601b7bcab3bbc01b58dec93
Luiz dos Reis <luizdosreis@gmail.com>
2019-09-01 22:24:06 -0300

    extends the default storybook webpack and add linter

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,14 @@
+const webpackConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js');
+
+module.exports = function(config, env) {
+  const newConfig = webpackConfig(config, env);
+
+  newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat({
+    test: /\.js$/,
+    exclude: /node_modules/,
+    include: /src/,
+    loader: 'eslint-loader',
+  });
+
+  return newConfig;
+};
