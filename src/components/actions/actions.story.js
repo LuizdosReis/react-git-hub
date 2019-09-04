49d99588e913d8d3683e8c0f5cdc0f0b79ef8c4a
Luiz dos Reis <luizdosreis@gmail.com>
2019-09-03 22:22:48 -0300

    rename history

--- src/components/actions/actions.story.js
+++ src/components/actions/actions.story.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Actions from './index';
 
-storiesOf('Actions', module).add('first history', () => (
+storiesOf('Actions', module).add('actions component', () => (
   <Actions getRepos={action('Get repositories')} getStarred={action('Get starred')} />
 ));
