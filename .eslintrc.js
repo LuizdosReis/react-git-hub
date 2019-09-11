ebaa79b72b319686e584f8a0b8b6bc75a2b48269
Luiz dos Reis <luizdosreis@gmail.com>
2019-09-10 22:51:21 -0300

    add global function to eslint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ["airbnb"],
+  extends: ['airbnb'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    test: 'readonly',
+    it: 'readonly',
+    describe: 'readonly',
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
   settings: {
-    "import/resolver": {
-      alias: [["src", "./src"], ["components", "./src/components"]]
-    }
-  }
+    'import/resolver': {
+      alias: [['src', './src'], ['components', './src/components']],
+    },
+  },
 };
