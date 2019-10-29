85d77df1d680934ad2c98f997757f619d46d460b
Luiz dos Reis <luizdosreis@gmail.com>
2019-10-28 23:03:15 -0300

    add script to update packages

--- scripts/update.js
+++ scripts/update.js
@@ -4,9 +4,9 @@ const pkg = require('../package.json');
 const dependencies = Object.keys(pkg.dependencies);
 const devDepencies = Object.keys(pkg.devDependencies);
 
-const add = args => spawn('yarn', ['add'].concat(args), { stdio: 'inherit' });
-const addDev = args => add(['--dev'].concat(args));
+const add = (args) => spawn('yarn', ['add'].concat(args), { stdio: 'inherit' });
+const addDev = (args) => add(['--dev'].concat(args));
 
 add(dependencies).on('close', () => {
-  addDev(devDepencies.concat('react-hot-loader@next')).on('close', code => process.exit(code));
+  addDev(devDepencies.concat('react-hot-loader@next')).on('close', (code) => process.exit(code));
 });
